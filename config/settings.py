fd54b4c8c0a59187a95b603e1fc2017308e92545
sumakanth092 <sumakanth092@gmail.com>
2025-10-18 18:46:36 +0530

    Cleaned DATABASES block for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import os
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-5_u9uopcs8lt2u#fs3p3ii7k2@*t0tfxm8cjt9xn$t-6e^iy^4'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["marketstore.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -51,13 +51,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
 AUTH_USER_MODEL = 'accounts.User'  # <-- Add this line here
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -75,5 +72,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
