6956a8b789ba50f709043ed15fd81622175535b7
sumakanth092 <sumakanth092@gmail.com>
2025-10-22 17:38:01 +0530

    Remove CustomUser admin registration, use default User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser  # if you have a custom user model
-admin.site.register(CustomUser, UserAdmin)
+from django.contrib.auth.models import User
+admin.site.register(User)
