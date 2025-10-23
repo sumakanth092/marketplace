251bba2db6d97fea3b221ecbaf2a551adf4cf008
sumakanth092 <sumakanth092@gmail.com>
2025-10-23 12:20:54 +0530

    fix: added profile_redirect view for custom profile redirect

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def seller_dashboard(request):
-    return render(request, 'accounts/seller_dashboard.html')
+def profile(request):
+    return render(request, 'registration/profile.html')
+
 
 @login_required
-def customer_dashboard(request):
-    return render(request, 'accounts/customer_dashboard.html')
+def profile_redirect(request):
+    user = request.user
+    if hasattr(user, 'is_seller') and user.is_seller:
+        return redirect('seller_dashboard')
+    else:
+        return redirect('customer_dashboard')
