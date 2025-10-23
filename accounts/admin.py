baef22954905910fbe5dfdbd179dfc93763c6918
sumakanth092 <sumakanth092@gmail.com>
2025-10-23 13:38:09 +0530

    fix: show is_seller field in admin user form

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,14 +5,23 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ['username', 'email', 'is_staff', 'is_seller']
-    list_filter = ['is_staff', 'is_seller', 'is_superuser']
-    search_fields = ['username', 'email']
 
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('is_seller',)}),
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_seller')
+    list_filter = ('is_staff', 'is_superuser', 'is_seller')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_seller', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('is_seller',)}),
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_seller'),
+        }),
     )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
