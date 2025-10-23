e388e1d1333742f47d18b57a345366699c5dfff4
sumakanth092 <sumakanth092@gmail.com>
2025-10-23 12:53:32 +0530

    show is_seller field in admin user form

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,8 +6,9 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ['username', 'email', 'is_staff', 'is_seller']
+    list_filter = ['is_staff', 'is_seller', 'is_superuser']
+    search_fields = ['username', 'email']
 
-    # Add custom fields to the "User" form in admin
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('is_seller',)}),
     )
